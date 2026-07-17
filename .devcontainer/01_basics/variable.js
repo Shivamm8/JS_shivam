b5b3fccf8df0617b7afce6b4c0cff6d89fb4e1ba
Shivamm8 <shivam282m@gmail.com>
2026-07-17 17:42:47 +0000

    done

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,10 @@
+const accountId=2345
+let accountEmail="shivam@gmail.com"
+var accountPass="123"
+accountState="jaipur"
+console.table([accountId, accountEmail, accountPass,accountState])
+accountEmail="shiva.com"
+accountPass="321"
+accountState="up"
+let accountCity;
+console.table([accountEmail, accountCity, accountPass, accountPass])
