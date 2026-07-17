a79e4bf3a8a1d9d1a89ecc39a169cce38fed824e
Shivamm8 <shivam282m@gmail.com>
2026-07-17 18:52:44 +0000

    all done

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,10 @@
+let age=18
+let name="shivam"
+let isLoggedIn=true
+//bigint(use for when you have very big number then we use)
+//string(use in dobule code)
+//boolean(user isloggedin or not means true or false)
+//null(stand alone value means empty state )
+//undefined(variable declare kr diye pr value assign nhi kiye)
+//symbol(use foe uniqueness)
+console.log(typeof age);
