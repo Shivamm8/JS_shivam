1c81ab7c96bd304593b746f69611e825063627c1
Shivamm8 <shivam282m@gmail.com>
2026-07-18 07:09:31 +0000

    conversion

---
+++ .devcontainer/01_basics/conversion.js
@@ -0,0 +1,19 @@
+let score =undefined
+console.log(typeof score)
+console.log(typeof (score))
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//"22"=22
+//"22ab"=nan
+//true=1 and false=0
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//1=true
+//0=false
+//""=false(empty)
+//"shivam"=true
+let somenumber =33
+let stringNumber=String(somenumber)
+console.log(typeof stringNumber)
