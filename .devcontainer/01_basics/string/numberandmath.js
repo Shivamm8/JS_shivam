025dd338937fc115caeb8c3d3b04b2c7a27d6e13
Shivamm8 <shivam282m@gmail.com>
2026-07-20 06:33:36 +0000

    mathdone

---
+++ .devcontainer/01_basics/string/numberandmath.js
@@ -0,0 +1,16 @@
+const score=21
+console.log(score)
+const balance = new Number(21)
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toFixed(1))
+const othernumber=1234.564
+console.log(othernumber.toPrecision(5))
+//////math
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.7))
+//ceil means agr 4 ke upr ho koi bhi value ho to 4 se bdi value lega
+//floor me chahe jitni bdi ho lowest value  hi lega
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
