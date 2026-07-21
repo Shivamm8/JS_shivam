4c2f5f96f06d2e2a78515f523db66c79ba21fcf1
Shivamm8 <shivam282m@gmail.com>
2026-07-21 06:27:08 +0000

    after do some changes

--- .devcontainer/01_basics/string/array/basicarray.js
+++ .devcontainer/01_basics/string/array/basicarray.js
@@ -17,3 +17,18 @@ const a=myAaray.splice(2,4)
 console.log(a)
 console.log(myAaray)
 // in alice our original array does'nt change but in aplice our original array change
+const marvel_heros=["ram","shyam"]
+const dc_heros=["charan","laxman"]
+const new_heros=[...marvel_heros,...dc_heros]
+// known as spread operator we regularly use
+console.log(new_heros)
+const another_array=[1,3,2,[4,5],[6,7,[8,9]]]
+const other_array=another_array.flat(Infinity)
+// flat use for the combining array in one array
+console.log(other_array)
+console.log(Array.isArray("shivam"))
+console.log(Array.from("shivam"))
+console.log(Array.from({name:"shivam"}))
+//hme dena padega kya convert krna chahte h
+const crr=Array.of("fal","tru","none")
+console.log(crr)
